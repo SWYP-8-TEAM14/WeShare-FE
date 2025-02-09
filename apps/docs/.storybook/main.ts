aae57ea1968b12cad03cdd43ce92d80cf59497bd
qaws7791 <qaws7791@gmail.com>
2025-02-10 01:15:48 +0900

    feat: Storybook 접근성 애드온 추가

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -15,6 +15,7 @@ const config: StorybookConfig = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
+    getAbsolutePath("@storybook/addon-a11y"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),

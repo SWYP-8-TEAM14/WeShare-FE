615fa8af16e41ef305ab81a9106c829e2ba3dd0b
qaws7791 <qaws7791@gmail.com>
2025-02-13 17:48:26 +0900

    feat: textarea 컴포넌트 추가

---
+++ packages/ui/src/textarea.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+import { tv, VariantProps } from "tailwind-variants";
+import { focusVisibleRing } from "./rings";
+
+export const textareaStyles = tv({
+  extend: focusVisibleRing,
+  base: "ui:flex ui:min-h-13.5 ui:w-full ui:py-4 ui:px-4.5 ui:rounded-sm ui:text-body-2 ui:placeholder:text-gray-500 ui:bg-gray-100",
+  variants: {
+    resize: {
+      true: "",
+      false: "ui:resize-none",
+    },
+  },
+  defaultVariants: {
+    resize: true,
+  },
+});
+
+export type TextareaProps = React.ComponentPropsWithRef<"textarea"> &
+  VariantProps<typeof textareaStyles>;
+
+export const Textarea = React.forwardRef<
+  React.ComponentRef<"textarea">,
+  TextareaProps
+>(({ className, resize, ...props }, ref) => {
+  return (
+    <textarea
+      className={textareaStyles({ className, resize })}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Textarea.displayName = "Textarea";

75719c9efce9f33330ae852fb40b932d34e05a2b
qaws7791 <qaws7791@gmail.com>
2025-02-14 23:30:25 +0900

    feat: 리스트 헤더 컴포넌트 추가

---
+++ packages/ui/src/list-header.tsx
@@ -0,0 +1,48 @@
+/**
+ * List header component
+ * 리스트 헤더 컴포넌트는 리스트의 그룹 맨 위에 배치되어 리스트 그룹을 구분하고, 리스트 그룹의 제목을 표시합니다. 우측에 버튼을 배치하여 리스트 그룹에 대한 추가 작업(예: 리스트 더보기)을 수행할 수 있습니다.
+ */
+
+import * as React from "react";
+import { cn } from "./utils";
+
+export const ListHeader = React.forwardRef<
+  React.ComponentRef<"div">,
+  React.ComponentPropsWithoutRef<"div">
+>(function ListHeader({ className, ...props }, ref) {
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "ui:px-4.5 ui:flex ui:justify-between ui:items-center",
+        className
+      )}
+      {...props}
+    />
+  );
+});
+ListHeader.displayName = "ListHeader";
+
+export const ListHeaderTitle = React.forwardRef<
+  React.ComponentRef<"div">,
+  React.ComponentPropsWithoutRef<"div">
+>(function ListHeaderTitle({ className, ...props }, ref) {
+  return (
+    <div ref={ref} className={cn("ui:text-heading-3", className)} {...props} />
+  );
+});
+ListHeaderTitle.displayName = "ListHeaderTitle";
+
+export const ListHeaderAction = React.forwardRef<
+  React.ComponentRef<"div">,
+  React.ComponentPropsWithoutRef<"div">
+>(function ListHeaderAction({ className, ...props }, ref) {
+  return (
+    <div
+      ref={ref}
+      className={cn("ui:text-body-6 ui:text-gray-700", className)}
+      {...props}
+    />
+  );
+});
+ListHeaderAction.displayName = "ListHeaderAction";

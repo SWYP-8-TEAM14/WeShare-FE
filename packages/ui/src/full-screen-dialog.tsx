e9540bcc9918761968151c38ade9c9f9e685cfdd
qaws7791 <qaws7791@gmail.com>
2025-02-18 22:28:00 +0900

    feat: 전체 화면 대화상자 컴포넌트 추가

---
+++ packages/ui/src/full-screen-dialog.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import * as React from "react";
+
+import { cn } from "./utils";
+
+const FullScreenDialog = DialogPrimitive.Root;
+
+const FullScreenDialogTrigger = DialogPrimitive.Trigger;
+
+const FullScreenDialogPortal = DialogPrimitive.Portal;
+
+const FullScreenDialogClose = DialogPrimitive.Close;
+
+const FullScreenDialogOverlay = React.forwardRef<
+  React.ComponentRef<typeof DialogPrimitive.Overlay>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Overlay
+    ref={ref}
+    className={cn("ui:fixed ui:inset-0 ui:z-50 ui:bg-black/50", className)}
+    {...props}
+  />
+));
+FullScreenDialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
+
+const FullScreenDialogContent = React.forwardRef<
+  React.ComponentRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <FullScreenDialogPortal>
+    <FullScreenDialogOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        "ui:fixed ui:left-1/2 ui:top-0 ui:h-full ui:z-50 ui:grid ui:w-full ui:max-w-sm  ui:-translate-x-1/2 ui:gap-4 ui:bg-white",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </DialogPrimitive.Content>
+  </FullScreenDialogPortal>
+));
+FullScreenDialogContent.displayName = DialogPrimitive.Content.displayName;
+
+const FullScreenDialogTitle = React.forwardRef<
+  React.ComponentRef<typeof DialogPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Title
+    ref={ref}
+    className={cn("ui:text-heading-4", className)}
+    {...props}
+  />
+));
+FullScreenDialogTitle.displayName = DialogPrimitive.Title.displayName;
+
+export {
+  FullScreenDialog,
+  FullScreenDialogClose,
+  FullScreenDialogContent,
+  FullScreenDialogTitle,
+  FullScreenDialogTrigger,
+};

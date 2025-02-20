e273f03c0394f43c759ee7358d9b537c8558dd2d
qaws7791 <qaws7791@gmail.com>
2025-02-20 23:41:49 +0900

    feat:  변경된 Textarea에 맞게 스토리 수정

--- apps/docs/stories/textarea.stories.tsx
+++ apps/docs/stories/textarea.stories.tsx
@@ -1,5 +1,3 @@
-import { Description, ErrorMessage, InputLength } from "@repo/ui/form";
-import { Label } from "@repo/ui/label";
 import { Textarea } from "@repo/ui/textarea";
 import type { Meta, StoryObj } from "@storybook/react";
 
@@ -17,15 +15,10 @@ const meta = {
     },
   },
   args: {
-    placeholder: "텍스트를 입력해 주세요",
-    resize: false,
-  },
-  argTypes: {
-    value: {
-      control: {
-        type: "text",
-      },
-    },
+    placeholder: "텍스트를 입력해 주세요.",
+    label: "라벨",
+    description: "텍스트를 입력해 주세요.",
+    maxLength: 100,
   },
   tags: ["autodocs"],
 } satisfies Meta<typeof Textarea>;
@@ -37,56 +30,6 @@ export const Default: Story = {
   render: (args) => <Textarea {...args} />,
 };
 
-export const WithLabel: Story = {
-  render: (args) => (
-    <div
-      style={{
-        display: "flex",
-        padding: "1rem",
-        flexDirection: "column",
-        gap: "0.5rem",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          gap: "0.5rem",
-        }}
-      >
-        <Label htmlFor="textarea">라벨</Label>
-        <InputLength maxLength={100} currentLength={0} />
-      </div>
-      <Textarea {...args} id="textarea" />
-      <Description>설명입니다</Description>
-    </div>
-  ),
-};
-
-export const WithError: Story = {
-  render: (args) => (
-    <div
-      style={{
-        display: "flex",
-        padding: "1rem",
-        flexDirection: "column",
-        gap: "0.5rem",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          gap: "0.5rem",
-        }}
-      >
-        <Label htmlFor="input">라벨</Label>
-        <InputLength maxLength={50} currentLength={4} />
-      </div>
-      <Textarea id="input" value="abcd" {...args} />
-      <ErrorMessage>최소 5자 이상 입력해 주세요.</ErrorMessage>
-    </div>
-  ),
+export const ErrorMessage: Story = {
+  render: (args) => <Textarea {...args} errorMessage="에러 메시지입니다." />,
 };

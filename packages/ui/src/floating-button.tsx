ed951cd5e723befefb103177ef324f0527afc6dc
qaws7791 <qaws7791@gmail.com>
2025-02-21 02:55:01 +0900

    feat: FloatingButton 컴포넌트를 absolute에서 일반 요소로 변경

--- packages/ui/src/floating-button.tsx
+++ packages/ui/src/floating-button.tsx
@@ -6,7 +6,7 @@ import { RadixUISlot } from "./types";
 import { tv } from "./utils";
 export const floatingButtonStyles = tv({
   extend: focusVisibleRing,
-  base: "ui:whitespace-nowrap ui:rounded-full ui:text-heading-5 ui:py-3.5 ui:px-4.5 ui:text-white ui:bg-primary-900 ui:shadow-[0_4px_10px_0_rgba(0,0,0,0.1)] ui:flex ui:gap-1 ui:items-center ui:absolute ui:-top-5  ui:left-1/2 ui:-translate-x-1/2 ui:-translate-y-full",
+  base: "ui:whitespace-nowrap ui:rounded-full ui:text-heading-5 ui:py-3.5 ui:px-4.5 ui:text-white ui:bg-primary-900 ui:shadow-[0_4px_10px_0_rgba(0,0,0,0.1)] ui:flex ui:gap-1 ui:items-center",
 });
 
 export type ButtonProps = React.ComponentPropsWithRef<"button"> &
@@ -19,13 +19,15 @@ export const FloatingButton = React.forwardRef<
 >(({ className, asChild, children, ...props }, ref) => {
   const Component = asChild ? Slot : "button";
   return (
-    <Component
-      ref={ref}
-      className={floatingButtonStyles({ className })}
-      {...props}
-    >
-      {children}
-    </Component>
+    <div className="ui:pb-5 ui:flex ui:justify-center">
+      <Component
+        ref={ref}
+        className={floatingButtonStyles({ className })}
+        {...props}
+      >
+        {children}
+      </Component>
+    </div>
   );
 });
 FloatingButton.displayName = "FloatingButton";

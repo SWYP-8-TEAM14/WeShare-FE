a8d5bea6df4d1503c6701bec3a4531c93eaa6199
qaws7791 <qaws7791@gmail.com>
2025-02-20 23:40:53 +0900

    feat: Textarea 컴포넌트에 레이블, 설명 및 오류 메시지 추가

--- packages/ui/src/textarea.tsx
+++ packages/ui/src/textarea.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 
 import { tv, VariantProps } from "tailwind-variants";
+import { Description, ErrorMessage, InputLength } from "./form";
 import { focusVisibleRing } from "./rings";
+import { cn } from "./utils";
 
 export const textareaStyles = tv({
   extend: focusVisibleRing,
@@ -13,23 +15,61 @@ export const textareaStyles = tv({
     },
   },
   defaultVariants: {
-    resize: true,
+    resize: false,
   },
 });
 
 export type TextareaProps = React.ComponentPropsWithRef<"textarea"> &
-  VariantProps<typeof textareaStyles>;
+  VariantProps<typeof textareaStyles> & {
+    label: string;
+    description?: string;
+    errorMessage?: string;
+  };
 
 export const Textarea = React.forwardRef<
   React.ComponentRef<"textarea">,
   TextareaProps
->(({ className, resize, ...props }, ref) => {
-  return (
-    <textarea
-      className={textareaStyles({ className, resize })}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+>(
+  (
+    {
+      className,
+      resize,
+      label,
+      description,
+      errorMessage,
+      maxLength,
+      ...props
+    },
+    ref
+  ) => {
+    const id = React.useId();
+    const [value, setValue] = React.useState(props.value || "");
+    const valueLength = typeof value === "string" ? value.length : 0;
+    return (
+      <div className="ui:flex ui:flex-col ui:gap-2 ui:w-full">
+        <div className="ui:flex ui:flex-row ui:justify-between ui:gap-2">
+          <label className="text-heading-5" htmlFor={"textarea-" + id}>
+            {label}
+          </label>
+          {maxLength && (
+            <InputLength maxLength={maxLength} currentLength={valueLength} />
+          )}
+        </div>
+        <textarea
+          id={"textarea-" + id}
+          ref={ref}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className={cn(textareaStyles({ className, resize }))}
+          {...props}
+        />
+        {errorMessage ? (
+          <ErrorMessage>{errorMessage}</ErrorMessage>
+        ) : description ? (
+          <Description>{description}</Description>
+        ) : null}
+      </div>
+    );
+  }
+);
 Textarea.displayName = "Textarea";

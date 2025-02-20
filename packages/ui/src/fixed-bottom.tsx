971bc1f693d28b9cc4f16d87fd5495d90723a3a1
qaws7791 <qaws7791@gmail.com>
2025-02-20 23:41:18 +0900

    feat: FixedBottomActions 컴포넌트 추가 및

--- packages/ui/src/fixed-bottom.tsx
+++ packages/ui/src/fixed-bottom.tsx
@@ -13,3 +13,27 @@ export const FixedBottom = ({
   );
 };
 FixedBottom.displayName = "FixedBottom";
+
+type FixedBottomActionsProps = React.ComponentPropsWithoutRef<"div"> & {
+  columns?: "1" | "2";
+};
+
+export const FixedBottomActions = ({
+  children,
+  className,
+  columns = "1",
+  ...props
+}: FixedBottomActionsProps) => {
+  return (
+    <div
+      className={cn(
+        "ui:bg-white ui:px-4.5 ui:py-2.5 ui:border-t ui:border-gray-200 grid gap-2",
+        columns === "1" ? "grid-cols-1" : "grid-cols-3",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};

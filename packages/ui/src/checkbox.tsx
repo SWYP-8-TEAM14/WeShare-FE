0bc136cf900dbf56994c3f638dbdfedc3f3458c8
qaws7791 <qaws7791@gmail.com>
2025-02-21 00:29:52 +0900

    feat: Checkbox 컴포넌트에 아이콘 추가

--- packages/ui/src/checkbox.tsx
+++ packages/ui/src/checkbox.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { CheckLargeIcon, CheckSmallIcon } from "@repo/icons";
 import * as React from "react";
 import { VariantProps } from "tailwind-variants";
-import { cn, tv } from "./utils";
+import { tv } from "./utils";
 
 export const checkboxStyles = tv({
   base: "ui:rounded-xs ui:data-[state=unchecked]:shadow-[inset_0_0_0_1px_var(--ui-color-gray-200)] ui:bg-white ui:data-[state=checked]:bg-primary-900 ui:text-gray-200 ui:data-[state=checked]:text-white",
@@ -32,43 +33,11 @@ export const Checkbox = React.forwardRef<
     className={checkboxStyles({ size, className })}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn("ui:flex ui:items-center ui:justify-center text-current")}
-    >
-      {size == "md" ? (
-        <svg
-          width="13"
-          height="9"
-          viewBox="0 0 13 9"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1 4.5L4.5 8L11.5 1"
-            stroke="currentColor"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      ) : (
-        <svg
-          width="10"
-          height="7"
-          viewBox="0 0 10 7"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1.25 3.375L3.875 6L9.125 0.75"
-            stroke="currentColor"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      )}
-    </CheckboxPrimitive.Indicator>
+    {size == "md" ? (
+      <CheckLargeIcon />
+    ) : (
+      <CheckSmallIcon className="size-4.5" />
+    )}
   </CheckboxPrimitive.Root>
 ));
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;

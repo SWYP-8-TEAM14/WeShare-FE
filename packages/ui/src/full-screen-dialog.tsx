27785fd5bebb1be32f02439f7c188855b7a8ef09
qaws7791 <qaws7791@gmail.com>
2025-02-20 23:41:03 +0900

    feat: FullScreenDialog에서 외부 상호작용 방지 기능 추가

--- packages/ui/src/full-screen-dialog.tsx
+++ packages/ui/src/full-screen-dialog.tsx
@@ -13,28 +13,20 @@ const FullScreenDialogPortal = DialogPrimitive.Portal;
 
 const FullScreenDialogClose = DialogPrimitive.Close;
 
-const FullScreenDialogOverlay = React.forwardRef<
-  React.ComponentRef<typeof DialogPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <DialogPrimitive.Overlay
-    ref={ref}
-    className={cn("ui:fixed ui:inset-0 ui:z-50 ui:bg-black/50", className)}
-    {...props}
-  />
-));
-FullScreenDialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
-
 const FullScreenDialogContent = React.forwardRef<
   React.ComponentRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+>(({ className, children, onInteractOutside, ...props }, ref) => (
   <FullScreenDialogPortal>
-    <FullScreenDialogOverlay />
     <DialogPrimitive.Content
       ref={ref}
+      onInteractOutside={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        onInteractOutside?.(e);
+      }}
       className={cn(
-        "ui:fixed ui:left-1/2 ui:top-0 ui:h-full ui:z-50 ui:grid ui:w-full ui:max-w-sm  ui:-translate-x-1/2 ui:gap-4 ui:bg-white",
+        "ui:fixed ui:left-1/2 ui:top-0 ui:h-full ui:z-50  ui:w-full ui:max-w-sm  ui:-translate-x-1/2 ui:bg-white",
         className
       )}
       {...props}

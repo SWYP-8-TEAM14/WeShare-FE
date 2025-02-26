15fca73d6603cdb40ee40470cc031b17ad52d5eb
qaws7791 <qaws7791@gmail.com>
2025-02-27 00:48:44 +0900

    feat: CreateItemForm에 픽업 및 반납 장소, 주의사항 필드 추가

--- apps/web/app/(service)/app/groups/[groupId]/manage/new-item/_components/create-item-form.tsx
+++ apps/web/app/(service)/app/groups/[groupId]/manage/new-item/_components/create-item-form.tsx
@@ -17,9 +17,6 @@ export default function CreateItemForm() {
   const itemForm = useItemForm({
     defaultValues: {
       images: [],
-      name: "",
-      quantity: 1,
-      description: "",
     },
   });
   const imagesPreview = useFilePreviews({
@@ -33,17 +30,17 @@ export default function CreateItemForm() {
   return (
     <>
       <form className="flex-1 bg-white" id={`form-${id}`} onSubmit={onSubmit}>
-        <div className="py-6.5 flex flex-col px-4.5">
+        {/* 사진 업로드 */}
+        <div className="pt-6.5 pb-2 px-4.5 flex flex-col">
           <div>
             <span className="text-heading-5">사진 등록</span>
             <p className="text-body-5 text-gray-600">
               공용물품 사진을 최대 4개 등록해 주세요.
             </p>
           </div>
-          {/* 사진 업로드 */}
           <div className="mt-4 flex gap-2">
             {itemForm.getValues("images").length < 4 && (
-              <label className="pt-2.5 w-20 h-20 flex flex-col items-center border border-gray-500 rounded-sm">
+              <label className="pt-2.5 w-20 h-20 flex flex-col items-center border border-black/10 rounded-sm">
                 <AddPhotoslotIcon className="size-4.5 text-gray-500" />
                 <span className="text-body-6 mt-1.5">사진 업로드</span>
                 <span className="text-detail-3 text-gray-700 mt-0.5">
@@ -101,28 +98,55 @@ export default function CreateItemForm() {
               </div>
             ))}
           </div>
+        </div>
 
-          {/* 정보 입력 */}
-          <div className="pt-8.5 flex flex-col gap-7.5">
-            <TextField
-              label="물품 이름"
-              placeholder="물품 이름을 작성해 주세요."
-            />
-            <TextField
-              className="text-right"
-              type="number"
-              label="물품 수량"
-              min={1}
-              max={999}
-              step={1}
-              endContent={<span className="text-body-2 text-gray-700">개</span>}
-            />
-            <Textarea
-              label="상세 내용"
-              maxLength={100}
-              placeholder="물품에 대한 설명을 작성해 주세요."
-            />
-          </div>
+        {/* 정보 입력 */}
+        <div className="pt-6.5 pb-7.5 px-4.5 flex flex-col gap-7.5">
+          <TextField
+            label="물품 이름"
+            placeholder="물품 이름을 작성해 주세요."
+            {...itemForm.register("name")}
+          />
+          <TextField
+            className="text-right"
+            type="number"
+            label="물품 수량"
+            placeholder="0"
+            min={1}
+            max={999}
+            step={1}
+            endContent={<span className="text-body-2 text-gray-700">개</span>}
+            {...itemForm.register("quantity")}
+          />
+          <Textarea
+            label="상세 내용"
+            maxLength={100}
+            placeholder="물품에 대한 설명을 작성해 주세요."
+            {...itemForm.register("description")}
+          />
+        </div>
+        <hr className="h-2 bg-gray-100 border-none" />
+        {/* 장소 입력 */}
+        <div className="py-7.5 px-4.5 flex flex-col gap-7.5">
+          <TextField
+            label="픽업 장소"
+            placeholder="픽업 장소를 작성해 주세요."
+            {...itemForm.register("pickupLocation")}
+          />
+          <TextField
+            label="반납 장소"
+            placeholder="반납 장소를 작성해 주세요."
+            {...itemForm.register("returnLocation")}
+          />
+        </div>
+        {/* 기타사항 입력 */}
+        <div className="pt-7.5 pb-6.5 px-4.5">
+          <Textarea
+            label="대여시 주의사항"
+            maxLength={100}
+            placeholder="대여시 주의사항을 작성해 주세요."
+            {...itemForm.register("caution")}
+          />
         </div>
       </form>
       <FixedBottom>

029b9758eba0d2368829dcd8df12a6d9b80ebe80
qaws7791 <qaws7791@gmail.com>
2025-02-27 16:02:09 +0900

    feat: MockGroups에서 그룹 목록 가져오기

--- apps/web/src/domains/group/repositories/group-repository.ts
+++ apps/web/src/domains/group/repositories/group-repository.ts
@@ -1,3 +1,4 @@
+import { MockGroups } from "@/domains/group/mocks";
 import httpClient from "@/lib/ky";
 
 export class GroupRepository {
@@ -12,5 +13,54 @@ export class GroupRepository {
       })
       .json();
     return response;
+  }
+
+  static async fetchGroups({
+    filter,
+    sort,
+    page,
+    limit,
+  }: {
+    filter: "all" | "own";
+    sort: "default" | "many-members";
+    page: number;
+    limit: number;
+  }) {
+    return MockGroups.groups
+      .filter((group) => {
+        if (filter === "all") {
+          return true;
+        }
+        return group.user.isOwner;
+      })
+      .sort((a, b) => {
+        if (sort === "default") {
+          return 0;
+        }
+        return b.members - a.members;
+      })
+      .slice((page - 1) * limit, page * limit);
+    // const response = (await httpClient
+    //   .get("groups", {
+    //     searchParams: {
+    //       filter,
+    //       sort,
+    //       page,
+    //       limit,
+    //     },
+    //   })
+    //   .json()) as {
+    //   id: number;
+    //   name: string;
+    //   image: string;
+    //   introduction: string;
+    //   members: number;
+    //   created_at: string;
+    //   updated_at: string;
+    //   user: {
+    //     isOwner: boolean;
+    //   };
+    // }[];
+    // return response;
   }
 }

9c20b0f1dd83c1eb5baa5df3ef8657246ff12630
qaws7791 <qaws7791@gmail.com>
2025-02-27 16:02:15 +0900

    feat: 그룹 목록 가져오기 기능 추가

--- apps/web/src/domains/group/services/group-service.ts
+++ apps/web/src/domains/group/services/group-service.ts
@@ -9,4 +9,24 @@ export class GroupService {
     const response = await GroupRepository.createGroup(data);
     return response;
   }
+
+  static async fetchGroups({
+    filter,
+    sort,
+    page,
+    limit,
+  }: {
+    filter: "all" | "own";
+    sort: "default" | "many-members";
+    page: number;
+    limit: number;
+  }) {
+    const response = await GroupRepository.fetchGroups({
+      filter,
+      sort,
+      page,
+      limit,
+    });
+    return response;
+  }
 }

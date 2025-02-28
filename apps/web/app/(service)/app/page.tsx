978b87b7dacfcee59e45b9bee74fde71f292c473
qaws7791 <qaws7791@gmail.com>
2025-02-28 16:05:53 +0900

    feat: 최대 6개의 아이템만 표시

--- apps/web/app/(service)/app/page.tsx
+++ apps/web/app/(service)/app/page.tsx
@@ -1,6 +1,6 @@
 import BottomNavigation from "@/components/bottom-navigation";
 import Page from "@/components/page";
-import { bookableItems } from "@/domains/item/mocks";
+import { groupItems } from "@/domains/item/mocks";
 import BookableItems from "./_components/bookable-items";
 import Header from "./_components/header";
 import MainBanners from "./_components/main-banners";
@@ -10,7 +10,7 @@ export default function AppPage() {
     <Page>
       <Header />
       <MainBanners pickupCount={1} returnCount={2} />
-      <BookableItems items={bookableItems} />
+      <BookableItems items={groupItems.slice(0, 6)} />
       <BottomNavigation currentTab="home" />
     </Page>
   );

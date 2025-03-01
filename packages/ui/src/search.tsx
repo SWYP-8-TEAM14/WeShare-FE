8a51a8434b5fc39069d45a04010bb150529cb32c
qaws7791 <qaws7791@gmail.com>
2025-03-02 01:35:33 +0900

    fix: 검색 컴포넌트에서 클리어 버튼 표시 안되는 부분 수정

--- packages/ui/src/search.tsx
+++ packages/ui/src/search.tsx
@@ -19,28 +19,40 @@ export const Search = React.forwardRef<
   React.ComponentRef<"input">,
   SearchProps
 >(({ className, onClear, ...props }, ref) => {
-  const [value, setValue] = React.useState(props.value || "");
+  const innerRef = React.useRef<HTMLInputElement>(null);
+  const [showClear, setShowClear] = React.useState(false);
 
+  React.useImperativeHandle(ref, () => innerRef.current!);
   React.useEffect(() => {
-    setValue(props.value || "");
-  }, [props.value]);
+    console.log("value", innerRef.current?.value);
+    if (innerRef.current?.value.length) {
+      setShowClear(true);
+    }
+  });
+
+  console.log(showClear, !!onClear);
 
   return (
     <div className="ui:relative">
       <SearchIcon className="ui:absolute ui:left-4.5 ui:top-1/2 ui:-translate-y-1/2 ui:text-gray-900" />
-      <input ref={ref} className={cn(searchStyles({ className }))} {...props} />
+      <input
+        ref={innerRef}
+        className={cn(searchStyles({ className }))}
+        {...props}
+      />
 
-      {value && onClear && (
+      {showClear && onClear && (
         <button
           type="button"
-          className="ui:absolute ui:right-[9px] ui:top-1/2 ui:-translate-y-1/2"
+          className="ui:absolute ui:right-[9px] ui:top-1/2 ui:-translate-y-1/2 ui:size-6 ui:flex ui:items-center ui:justify-center"
           aria-label="Clear value"
           onClick={() => {
-            setValue("");
-            onClear?.();
+            innerRef.current!.value = "";
+            setShowClear(false);
+            onClear();
           }}
         >
-          <DeletePhotoslotIcon className="ui:text-gray-600" />
+          <DeletePhotoslotIcon className="ui:text-gray-600 ui:size-4.5" />
         </button>
       )}
     </div>

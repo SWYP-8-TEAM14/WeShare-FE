c0f380c405da7198a28c1a3d7ee587dcbd7d9609
qaws7791 <qaws7791@gmail.com>
2025-03-05 20:49:07 +0900

    feat: 예약 페이지에서 아이템 상세 정보를 API로부터 가져오도록 수정

--- apps/web/app/(service)/app/reserve/[itemId]/_components/reservation-form.tsx
+++ apps/web/app/(service)/app/reserve/[itemId]/_components/reservation-form.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { itemDetail } from "@/domains/item/mocks";
 import SelectReservationTimeDialog from "@/domains/reservation/components/select-reservation-time-dialog";
 import { useReservationForm } from "@/domains/reservation/hooks/use-reservation-form";
 import { useReserveItem } from "@/domains/reservation/hooks/use-reserve-item";
@@ -22,7 +21,26 @@ import dayjs from "dayjs";
 import Image from "next/image";
 import { Suspense } from "react";
 
-export default function ReservationForm() {
+interface ReservationFormProps {
+  itemDetail: {
+    userId: number;
+    username: string;
+    groupId: number;
+    groupName: string;
+    itemId: number;
+    itemName: string;
+    pickupPlace: string;
+    returnPlace: string;
+    itemDescription: string;
+    imageUrls: string[];
+    status: number;
+    quantity: number;
+    caution: string;
+    createdAt: string;
+  };
+}
+
+export default function ReservationForm({ itemDetail }: ReservationFormProps) {
   const formId = useFormId();
   const reserveItem = useReserveItem();
   const reservationForm = useReservationForm();
@@ -44,7 +62,7 @@ export default function ReservationForm() {
       <div className="bg-white px-4.5 py-6.5 flex items-center gap-3">
         <Image
           unoptimized
-          src={itemDetail.images[0]!}
+          src={itemDetail.imageUrls[0]!}
           alt={itemDetail.itemName}
           width={74}
           height={74}
@@ -53,7 +71,7 @@ export default function ReservationForm() {
 
         <div className="flex flex-col gap-1.5">
           <span className="text-body-6 text-gray-600">
-            {itemDetail.group.name}
+            {itemDetail.groupName}
           </span>
           <p className="text-body-1">{itemDetail.itemName}</p>
           <span className="text-body-4 text-gray-700">
@@ -66,13 +84,13 @@ export default function ReservationForm() {
           <div>
             <span className="text-body-6 text-gray-600">픽업 장소</span>
             <p className="text-heading-2 mt-1.5 text-gray-800">
-              {itemDetail.pickupLocation}
+              {itemDetail.pickupPlace}
             </p>
           </div>
           <div>
             <span className="text-body-6 text-gray-600">반납 장소</span>
             <p className="text-heading-2 mt-1.5 text-gray-800">
-              {itemDetail.returnLocation}
+              {itemDetail.returnPlace}
             </p>
           </div>
         </div>
@@ -91,7 +109,7 @@ export default function ReservationForm() {
               defaultTab="start-time"
               defaultStartTime={startTime}
               defaultEndTime={endTime}
-              itemId={itemDetail.id.toString()}
+              itemId={itemDetail.itemId.toString()}
               onConfirm={(times) => {
                 reservationForm.setValue("startTime", times.startTime);
                 reservationForm.setValue("endTime", times.endTime);
@@ -120,7 +138,7 @@ export default function ReservationForm() {
               defaultTab="end-time"
               defaultStartTime={startTime}
               defaultEndTime={endTime}
-              itemId={itemDetail.id.toString()}
+              itemId={itemDetail.itemId.toString()}
               onConfirm={(times) => {
                 reservationForm.setValue("startTime", times.startTime);
                 reservationForm.setValue("endTime", times.endTime);
@@ -187,7 +205,7 @@ export default function ReservationForm() {
                 <div className="bg-white flex items-center gap-3">
                   <Image
                     unoptimized
-                    src={itemDetail.images[0]!}
+                    src={itemDetail.imageUrls[0]!}
                     alt={itemDetail.itemName}
                     width={74}
                     height={74}
@@ -196,7 +214,7 @@ export default function ReservationForm() {
 
                   <div className="flex flex-col gap-1.5">
                     <span className="text-body-6 text-gray-600">
-                      {itemDetail.group.name}
+                      {itemDetail.groupName}
                     </span>
                     <p className="text-body-1">{itemDetail.itemName}</p>
                     <span className="text-body-4 text-gray-700">
--- apps/web/app/(service)/app/reserve/[itemId]/page.tsx
+++ apps/web/app/(service)/app/reserve/[itemId]/page.tsx
@@ -1,22 +1,34 @@
 import Page from "@/components/page";
 import RouterBackButton from "@/components/router-back-button";
+import { ItemService } from "@/domains/item/services/item-service";
 import {
   TopNavigation,
   TopNavigationLeft,
   TopNavigationTitle,
 } from "@repo/ui/top-navigation";
+import { notFound } from "next/navigation";
 import ReservationForm from "./_components/reservation-form";
 
-export default function ItemReservePage() {
-  return (
-    <Page>
-      <TopNavigation>
-        <TopNavigationLeft>
-          <RouterBackButton />
-        </TopNavigationLeft>
-        <TopNavigationTitle>예약하기</TopNavigationTitle>
-      </TopNavigation>
-      <ReservationForm />
-    </Page>
-  );
+export default async function ItemReservePage({
+  params,
+}: {
+  params: Promise<{ itemId: string }>;
+}) {
+  try {
+    const { itemId } = await params;
+    const item = await ItemService.fetchItem(itemId);
+    return (
+      <Page>
+        <TopNavigation>
+          <TopNavigationLeft>
+            <RouterBackButton />
+          </TopNavigationLeft>
+          <TopNavigationTitle>예약하기</TopNavigationTitle>
+        </TopNavigation>
+        <ReservationForm itemDetail={item} />
+      </Page>
+    );
+  } catch {
+    notFound();
+  }
 }

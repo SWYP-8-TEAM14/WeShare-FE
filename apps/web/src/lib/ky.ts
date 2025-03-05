e69247a3206b868670d3952eb55938c620ddb3de
qaws7791 <qaws7791@gmail.com>
2025-03-06 03:59:10 +0900

    feat: HTTP 클라이언트에 인증 토큰 추가 및 js-cookie 라이브러리 추가

--- apps/web/src/lib/ky.ts
+++ apps/web/src/lib/ky.ts
@@ -1,8 +1,20 @@
 import { clientEnv } from "@/lib/client-env";
+import { getAccessToken } from "@/lib/http-client";
 import ky from "ky";
 
 const httpClient = ky.create({
   prefixUrl: clientEnv.API_URL,
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        const token = await getAccessToken();
+        console.log(`token: ${token}`);
+        if (token) {
+          request.headers.set("Authorization", `Bearer ${token}`);
+        }
+      },
+    ],
+  },
 });
 
 export default httpClient;

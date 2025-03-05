8a4d3aac0e67ddfc8af7a97fc794e4b526f0b1ac
qaws7791 <qaws7791@gmail.com>
2025-03-06 06:46:27 +0900

    feat: HTTP 클라이언트에 Content-Length 및 인증 헤더 추가

--- apps/web/src/lib/ky.ts
+++ apps/web/src/lib/ky.ts
@@ -1,19 +1,29 @@
 import { clientEnv } from "@/lib/client-env";
 import { getAccessToken } from "@/lib/http-client";
-import ky from "ky";
+import ky, { BeforeRequestHook } from "ky";
+
+const addContentLengthHeader: BeforeRequestHook = async (request, options) => {
+  if (
+    options.body &&
+    !request.headers.has("Content-Length") &&
+    typeof options.body === "string"
+  ) {
+    const ContentLength = new TextEncoder().encode(options.body).length;
+    request.headers.set("Content-Length", ContentLength.toString());
+  }
+};
+
+const addAccessTokenHeader: BeforeRequestHook = async (request) => {
+  const accessToken = await getAccessToken();
+  if (accessToken) {
+    request.headers.set("Authorization", `Bearer ${accessToken}`);
+  }
+};
 
 const httpClient = ky.create({
   prefixUrl: clientEnv.API_URL,
   hooks: {
-    beforeRequest: [
-      async (request) => {
-        const token = await getAccessToken();
-        console.log(`token: ${token}`);
-        if (token) {
-          request.headers.set("Authorization", `Bearer ${token}`);
-        }
-      },
-    ],
+    beforeRequest: [addContentLengthHeader, addAccessTokenHeader],
   },
 });
 

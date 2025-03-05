f75f85b38e192fe4f3cf201cdefefec6c111a79b
qaws7791 <qaws7791@gmail.com>
2025-03-06 06:45:03 +0900

    feat: 콘솔 로그 제거

--- apps/web/src/lib/client-env.ts
+++ apps/web/src/lib/client-env.ts
@@ -6,7 +6,6 @@ export const clientEnvSchema = z.object({
 
 export type ClientEnv = z.infer<typeof clientEnvSchema>;
 
-console.log("process.env", process.env);
 export const clientEnv: ClientEnv = clientEnvSchema.parse({
   API_URL: process.env.NEXT_PUBLIC_API_URL,
 });

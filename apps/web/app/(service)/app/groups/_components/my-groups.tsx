5743ac9f566952f31542ca043a7ec8fd911ac395
qaws7791 <qaws7791@gmail.com>
2025-03-07 23:32:07 +0900

    fix: 내 그룹이 없을 때 바텀 네비게이션 누락된 문제 해결

--- apps/web/app/(service)/app/groups/_components/my-groups.tsx
+++ apps/web/app/(service)/app/groups/_components/my-groups.tsx
@@ -18,32 +18,37 @@ export default function MyGroups({ filter, sort }: MyGroupsProps) {
 
   if (groups.length === 0) {
     return (
-      <div className="flex-1 flex flex-col items-center justify-center">
-        <h1 className="sr-only">내 그룹</h1>
-        <div className="flex flex-col items-center">
-          <Image
-            unoptimized
-            src="/images/empty-groups.png"
-            width={100}
-            height={100}
-            alt="empty-groups"
-          />
-          <div className="text-center mt-4.5 max-w-44">
-            <span className="text-heading-4 text-center">
-              아직 내 그룹이 없어요!
-            </span>
-            <p className="text-body-5 text-gray-800 text-center  mt-2">
-              그룹을 만들어 멤버들과 함께 공용 물품을 관리해보세요!
-            </p>
+      <>
+        <div className="flex-1 flex flex-col items-center justify-center">
+          <h1 className="sr-only">내 그룹</h1>
+          <div className="flex flex-col items-center">
+            <Image
+              unoptimized
+              src="/images/empty-groups.png"
+              width={100}
+              height={100}
+              alt="empty-groups"
+            />
+            <div className="text-center mt-4.5 max-w-44">
+              <span className="text-heading-4 text-center">
+                아직 내 그룹이 없어요!
+              </span>
+              <p className="text-body-5 text-gray-800 text-center  mt-2">
+                그룹을 만들어 멤버들과 함께 공용 물품을 관리해보세요!
+              </p>
+            </div>
+            <Link
+              href={`/app/groups/new`}
+              className="mt-8 flex items-center justify-center rounded-full px-2.5 py-[13px] border border-black/10 bg-white w-[130px] text-body-3 text-gray-700"
+            >
+              그룹 생성하기
+            </Link>
           </div>
-          <Link
-            href={`/app/groups/new`}
-            className="mt-8 flex items-center justify-center rounded-full px-2.5 py-[13px] border border-black/10 bg-white w-[130px] text-body-3 text-gray-700"
-          >
-            그룹 생성하기
-          </Link>
         </div>
-      </div>
+        <FixedBottom>
+          <BottomNavigation currentTab="groups" />
+        </FixedBottom>
+      </>
     );
   } else {
     return (

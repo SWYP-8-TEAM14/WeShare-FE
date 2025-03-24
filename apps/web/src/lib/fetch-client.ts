cae2f3218ef12a5d92136f44bc9a989d9e14ce03
qaws7791 <qaws7791@gmail.com>
2025-03-24 22:08:46 +0900

    fetchClient 쿠키 미들웨어 추가

--- apps/web/src/lib/fetch-client.ts
+++ apps/web/src/lib/fetch-client.ts
@@ -1,8 +1,20 @@
 import { clientEnv } from "@/lib/client-env";
 import { paths } from "@/types/api-schema";
-import createClient from "openapi-fetch";
+import createClient, { Middleware } from "openapi-fetch";
+const cookieMiddleware: Middleware = {
+  async onRequest({ request, options }) {
+    if (typeof window == "undefined") {
+      const session = await import("next/headers").then((headers) =>
+        headers.cookies().then((cookies) => cookies.get("s_id")?.value)
+      );
 
+      request.headers.set("cookie", `s_id=${session}`);
+    }
+  },
+};
 export const fetchClient = createClient<paths>({
   baseUrl: clientEnv.API_URL,
   credentials: "include",
 });
+
+fetchClient.use(cookieMiddleware);
